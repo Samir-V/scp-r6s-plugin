a68181796163ea9a70ba4302e2f307c20c20eb2d
Samirs Vertahovs <samirs.vertahovs@gmail.com>
2024-07-15 17:38:44 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scp_r6s_plugin
+{
+    public class Class1
+    {
+    }
+}
